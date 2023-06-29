dac3e27a49767b56868d0ca05f3b5e1a7a1d489c
Laurent HINNEKINT <laurent.hinnekint@gmail.com>
2023-06-29 20:58:23 +0200

    NOTIFICATION DONE

--- js/script.js
+++ js/script.js
@@ -8,3 +8,19 @@
 // action.addEventListener('click', function () {
 //     nav.classList.toggle('open');
 // });
+
+const closePopInAdd = document.querySelector('.closePopInAdd');
+const popin_Success_Add  = document.querySelector('.popin_Success_Add');
+
+closePopInAdd.addEventListener('click', function() {
+    popin_Success_Add.style.opacity = '0';
+});
+
+
+
+const popin_Fail_Add  = document.querySelector('.popin_Fail_Add');
+
+
+closePopInAdd.addEventListener('click', function() {
+    popin_Fail_Add.style.opacity = '0';
+});
--- todo.php
+++ todo.php
@@ -9,46 +9,65 @@ require 'require/_bdd.php';
 
 <body>
 
-    <form action= "add.php" method="post" class="form_container">
-        <label for="add_description"><h2 class="form_title">Quelle tâche souhaitez-vous ajouter ?</h2></label>
+    <form action="add.php" method="post" class="form_container">
+        <label for="add_description">
+            <h2 class="form_title">Quelle tâche souhaitez-vous ajouter ?</h2>
+        </label>
         <div class="input_container">
-        <input class="input_field" type="text" name="description" id="add_description">
-        <input class="input_submit" type="submit" name="submit">
+            <input class="input_field" type="text" name="description" id="add_description">
+            <input class="input_submit" type="submit" name="submit">
         </div>
         <?php
-            if (array_key_exists('msgAdd', $_GET)) {
-                echo '<p>'. $_GET['msg'] . '</p>';
+        if (array_key_exists('msg', $_GET)) {
+            $message = $_GET['msg'];
+            if ($message == "Ok") {
+                echo    '<section class="popin_Success_Add">
+                            <div class="popin_Success_Add_box">
+                                <article class="popin_Success_Add_content">
+                                    <p class="closePopInAdd">❌</p>
+                                    <p class="popin_Success_Add_text"> Votre tâche a été crée avec succès ! <p>
+                                </article>
+                            </div>
+                        </section>';
+            } else if ($message == "NotOk") {
+                echo "<section class='popin_Fail_Add'>
+                        <div class='popin_Fail_Add_box'>
+                            <article class='popin_Fail_Add_content'>
+                                <p class='closePopInAdd'>❌</p>
+                                <p class='popin_Fail_Add_text'> Une erreur est survenu, votre tâche n'a pas été crée ! <p>
+                            </article>
+                        </div>
+                    </section>";
             }
+        }
         ?>
     </form>
 
-<?php
+    <?php
 
     $query = $dbCo->prepare("SELECT task_creation_date, task_description, status_name
                             FROM task t
                                 JOIN status s USING (ID_status)
                             ORDER BY task_creation_date DESC");
     $query->execute();
-    $result = $query->fetchAll() ;
-    // var_dump($result);
+    $result = $query->fetchAll();
 
 
 
-echo '<ul class="task">';
+    echo '<ul class="task">';
 
-foreach ($result as $task) {
-    echo '<div class="task_container">';
-    // echo '<p class ="task_ttl">' . $task['task_name'] . '</p>';
-    echo '<li class="task_box"><input type="checkbox" id="checkBoxDelete"></li>';
-    echo '<li class="task_list">'. $task['task_description'] .'</li>';
-    echo '<li class="task_list"><button class="task_btn">'. $task['status_name'] .'</button></li>';
-    echo '</div>';
-}
+    foreach ($result as $task) {
+        echo '<div class="task_container">';
+        // echo '<p class ="task_ttl">' . $task['task_name'] . '</p>';
+        echo '<li class="task_box"><input type="checkbox" id="checkBoxDelete"></li>';
+        echo '<li class="task_list">' . $task['task_description'] . '</li>';
+        echo '<li class="task_list"><button class="task_btn">' . $task['status_name'] . '</button></li>';
+        echo '</div>';
+    }
 
-echo '</ul>';
+    echo '</ul>';
 
-?>
-
+    ?>
 
 </body>
 
